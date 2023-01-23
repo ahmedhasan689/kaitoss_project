a683c6fcdee97d98b82bf2d5615d9cc27b5b1f36
ahmedhasan689 <ahm19edhasan@gmail.com>
2023-01-23 22:36:24 +0200

    User Seeder
    User => Admins
    Login Functionality

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('dashboard.index');
-});
+})->middleware(['auth']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
